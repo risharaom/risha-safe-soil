6f08e0af8ca4c51a6898a56e24ca51b1dcba2cbe
risharaom <risharaom@gmail.com>
2025-04-28 13:32:15 -0700

    Removed bottom icon and space from the top

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -35,7 +35,7 @@ const AppSidebar = () => {
         dispatch({ type: 'set', sidebarShow: visible })
       }}
     >
-      <CSidebarHeader className="border-bottom">
+      {/* <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
           <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
           <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
@@ -45,13 +45,13 @@ const AppSidebar = () => {
           dark
           onClick={() => dispatch({ type: 'set', sidebarShow: false })}
         />
-      </CSidebarHeader>
+      </CSidebarHeader> */}
       <AppSidebarNav items={navigation} />
-      <CSidebarFooter className="border-top d-none d-lg-flex">
+      {/* <CSidebarFooter className="border-top d-none d-lg-flex">
         <CSidebarToggler
           onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
         />
-      </CSidebarFooter>
+      </CSidebarFooter> */}
     </CSidebar>
   )
 }
