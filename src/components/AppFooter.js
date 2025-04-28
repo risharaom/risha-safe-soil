4b90c6700fca380a772aee13334685e784a24436
risharaom <risharaom@gmail.com>
2025-04-27 20:16:33 -0700

    Deleted footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,12 +5,6 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
-        </a>
-        <span className="ms-1">&copy; 2025 creativeLabs.</span>
-      </div>
-      <div className="ms-auto">
         
       </div>
     </CFooter>
